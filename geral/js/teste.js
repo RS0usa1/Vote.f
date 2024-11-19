9efe426d0daa97daae768f188061538612d39602
RS0usa1 <richardrrggts@gmail.com>
2024-11-19 17:21:25 -0300

    oi

--- geral/js/teste.js
+++ geral/js/teste.js
@@ -1,27 +1,26 @@
 const showAndHiddenSignup = (show) => {
-  const loginText = document.querySelector(".slide-title .login");
-  const loginForm = document.querySelector("form.login");
+const loginText = document.querySelector(".slide-title .login");
+const loginForm = document.querySelector("form.login");
 
-  if (show) {
+if (show) {
     loginForm.style.marginLeft = "-50%";
     loginText.style.marginLeft = "-50%";
-  } else {
+} else {
     loginForm.style.marginLeft = "0%";
     loginText.style.marginLeft = "0%";
-  }
+}
 };
 
 const handlerTab = (element, event) => {
-  if (event.srcElement.id === "signup") showAndHiddenSignup(true);
+if (event.srcElement.id === "signup") showAndHiddenSignup(true);
 
-  if (event.srcElement.id === "login") showAndHiddenSignup(false);
+if (event.srcElement.id === "login") showAndHiddenSignup(false);
 };
 
 const signupLink = document.querySelector("form .signup-link a");
 const signupBtn = document.querySelector("label.signup");
 
 signupLink.onclick = () => {
-  signupBtn.click();
-  return false;
+signupBtn.click();
+return false;
 };
-  
